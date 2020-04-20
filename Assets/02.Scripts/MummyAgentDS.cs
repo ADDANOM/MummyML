0b589153d94918143bbfc518bb5256b3fb60c3e5
Lee JaeHyun <myleje@gmail.com>
2020-04-20 09:19:28 +0900

    .

---
+++ Assets/02.Scripts/MummyAgentDS.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using MLAgents;
+
+public class MummyAgentDS : Agent
+{
+    //에이젼트 초기화
+    public override void Initialize()
+    {
+
+    }
+
+    //에피소드의 초기화 (훈련이 시작될때 마다 호출)
+    public override void OnEpisodeBegin()
+    {
+
+    }
+
+    //관측정보를 수집하고 브레인에 전달하는 역할
+    public override void CollectObservations(MLAgents.Sensors.VectorSensor sensor)
+    {
+
+    }
+}
